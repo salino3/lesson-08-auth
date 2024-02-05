b777aa50e642e859b087a42efb5bbd1b08ef6f07
salino3 <programacionflavio@gmail.com>
2024-02-05 12:37:12 +0100

    add jwt library

---
+++ src/common-app/models/user-session.ts
@@ -0,0 +1,4 @@
+
+export interface userSession {
+  id: string;
+};
--- src/pods/security/security.rest-api.ts
+++ src/pods/security/security.rest-api.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 import {userRepository} from '#dals/index.js';
+import {userSession} from '#common-app/models/index.js';
 
 export const securityApi = Router();
+
 
 securityApi.post('/login', async (req, res, next) => {
   try {
@@ -10,11 +13,16 @@ securityApi.post('/login', async (req, res, next) => {
     const user = await userRepository.getUserByEmailAndPassword(email, password);
 
     if(user) {
-      //? userSession.save(user)
+      const userSession: userSession = {
+        id: user._id.toHexString()
+      }
+      const secret = 'my-secret'; // TODO: Move it to .env file
+      const token = jwt.sign(userSession, secret, {
+        expiresIn: '1d',
+        algorithm: 'HS256'
+      });
+      res.send(`Bearer ${token}`);
 
-      // Create token with user info
-
-      // Send token
     }else {
       res.sendStatus(401);
     }
